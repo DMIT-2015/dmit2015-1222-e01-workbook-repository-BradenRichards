41bf94133cc8f0d3c75a611b87cdc4e14e4a21a2
Braden Richards <brichards11@nait.ca>
2023-02-08 19:25:17 -0700

    Pulled Sams example for class start

--- dmit2015-faces-demo/src/main/java/dmit2015/model/CanadianPersonalIncomeTaxRateManager.java
+++ dmit2015-faces-demo/src/main/java/dmit2015/model/CanadianPersonalIncomeTaxRateManager.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class CanadianPersonalIncomeTaxRateManager {
@@ -12,14 +13,44 @@ public class CanadianPersonalIncomeTaxRateManager {
     @Getter
     private List<CanadianPersonalIncomeTaxRate> incomeTaxRates;
 
-    public CanadianPersonalIncomeTaxRateManager(){
+    public int[] availableTaxYears() {
+        return incomeTaxRates.stream()
+                .map(e -> e.getTaxYear())
+                .distinct()
+                .sorted(Comparator.reverseOrder())
+                .mapToInt(e -> e)
+                .toArray();
+    }
+
+    public List<Integer> availableTaxYearList() {
+        return incomeTaxRates.stream()
+                .map(e -> e.getTaxYear())
+                .distinct()
+                .sorted(Comparator.reverseOrder())
+                .toList();
+    }
+
+
+    public CanadianPersonalIncomeTaxRateManager() {
         incomeTaxRates = new ArrayList<>();
-        try{
+        try {
             try (var reader = new BufferedReader(new InputStreamReader(
-                    getClass().getResourceAsStream("/data/csv/CanadianPersonalIncomeTaxRates.csv")
-            ))){
+                    getClass().getResourceAsStream("/data/csv/CanadianPersonalIncomeTaxRates.csv")))) {
+                // Skip the first line as it contains headers
+                reader.readLine();
+                String line;
+                while ( (line = reader.readLine()) != null) {
+                    var optionalCanadianPersonalIncomeTaxRate = CanadianPersonalIncomeTaxRate.parseCsv(line);
+                    if (optionalCanadianPersonalIncomeTaxRate.isPresent()) {
+                        CanadianPersonalIncomeTaxRate currentCanadianPersonalIncomeTaxRate
+                                = optionalCanadianPersonalIncomeTaxRate.orElseThrow();
+                        incomeTaxRates.add(currentCanadianPersonalIncomeTaxRate);
+                    }
+                }
 
             }
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
         }
     }
 }
